d9d278beab09282ef0ab4ef900b217c1af83c957
Ansh Verma <ansh.verma.av0608@gmail.com>
2026-06-17 21:10:33 +0530

    Add @types/cookie-parser to devDependencies and fix import typo in server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,20 +1,25 @@
-import expres from "express";
+import express from "express";
 import cors from "cors";
 import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import authRoutes from "./modules/auth/auth.routes"
 
-
-const app = expres();
+const app = express();
 
 
 app.use(cors());
 app.use(helmet());
-app.use(expres.json());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/health",(_,res)=>{
     res.json({
         status:"ok"
     });
 });
+
+app.use("/api.auth",authRoutes);
 
 app.listen(5000,()=>{
     console.log("api running on port 5000");
