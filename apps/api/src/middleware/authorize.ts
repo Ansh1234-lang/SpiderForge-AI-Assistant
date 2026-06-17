6fab4948b85758b42ea6d94be85708b6bba36c67
Ansh Verma <ansh.verma.av0608@gmail.com>
2026-06-17 20:58:37 +0530

    Implement authorization middleware to manage user role access

--- apps/api/src/middleware/authorize.ts
+++ apps/api/src/middleware/authorize.ts
@@ -1,1 +1,27 @@
-import 
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "./authenticate";
+
+
+export const authorize =
+    (...roles: string[]) =>
+        (
+            req: AuthRequest,
+            res: Response,
+            next: NextFunction
+        ) => {
+            if (!req.user) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized",
+                });
+            }
+            if (
+                !roles.includes(req.user.role)
+            ) {
+                return res.status(403).json({
+                    success: false,
+                    message: "forbidden",
+                })
+            }
+            next();
+        }
